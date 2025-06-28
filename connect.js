12f310e131063d5a000056aa224a53afbf8780fc
Yazan Yousef <86908063+yazanyousef25@users.noreply.github.com>
2025-06-28 20:06:45 +0300

    Update connect.js

--- connect.js
+++ connect.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const inputData = document.getElementById('inputData').value;
     
-    fetch('https://wireless-backend-3xw9.onrender.com', {
+    fetch('https://wireless-backend-wvva.onrender.com', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
